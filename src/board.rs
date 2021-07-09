2815cad08a6ecfba1cf306fe9a3aed4b061b68d5
Ian Pringle <ipringle2@liberty.edu>
2021-07-09 15:46:48 -0500

    Added way too much for one commit... :D
    - Setup rocket server
    - Added flagging and reveal functionality
    - Added export of state
    - Added export of board
    - Generating boards with salt + seed

--- src/board.rs
+++ src/board.rs
@@ -1,11 +1,16 @@
-use fmt;
+use petname;
 use rand;
+use rand_seeder::{Seeder, SipRng};
+use serde::Serialize;
+use std::fmt;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Serialize)]
 pub struct Cell {
+    #[serde(skip)]
     mine: bool,
     flag: bool,
     reveal: bool,
+    #[serde(skip)]
     neighbors: u8,
 }
 
@@ -19,19 +24,35 @@ impl Cell {
         }
     }
 
-    fn toggle_mine(mut self) -> Self {
+    fn toggle_mine(&mut self) {
         self.mine = !self.mine;
-        self
     }
 
-    fn toggle_flag(mut self) -> Self {
-        self.flag = !self.flag;
-        self
+    fn toggle_flag(&mut self) -> bool {
+        if self.reveal {
+            false
+        } else {
+            self.flag = !self.flag;
+            true
+        }
     }
 
-    fn toggle_reveal(mut self) -> Self {
-        self.reveal = !self.reveal;
-        self
+    fn set_reveal(&mut self, value: bool) -> bool {
+        if self.flag {
+            false
+        } else {
+            self.reveal = value;
+            true
+        }
+    }
+
+    fn toggle_reveal(&mut self) -> bool {
+        if self.flag {
+            false
+        } else {
+            self.reveal = !self.reveal;
+            true
+        }
     }
 
     fn with_mine(mut self) -> Self {
@@ -45,12 +66,13 @@ impl Cell {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize)]
 pub struct Board {
     cells: Vec<Cell>,
     width: usize,
     height: usize,
     difficulty: usize,
+    pub seed: String,
 }
 
 impl Board {
@@ -60,7 +82,70 @@ impl Board {
             width: 0,
             height: 0,
             difficulty: 0,
+            seed: "".to_string(),
         }
+    }
+
+    pub fn from(state: String) -> Board {
+        let mut string_state = state.split_whitespace();
+        let width: usize = string_state.next().unwrap().parse::<usize>().unwrap();
+        let height: usize = string_state.next().unwrap().parse::<usize>().unwrap();
+        let difficulty: usize = string_state.next().unwrap().parse::<usize>().unwrap();
+        let seed: String = string_state.next().unwrap().to_string();
+        let board: Vec<String> = string_state.into_iter().map(|x| x.to_string()).collect();
+        let mut b = Board::new()
+            .with_width(width)
+            .with_height(height)
+            .with_difficulty(difficulty)
+            .with_seed(seed)
+            .build();
+        for (i, cell) in board.iter().enumerate() {
+            let (reveal, flag): (bool, bool) = match cell.as_str() {
+                "00" => (false, false),
+                "01" => (false, true),
+                "10" => (true, false),
+                "11" => (true, true),
+                _ => (false, false),
+            };
+            b.cells[i].reveal = reveal;
+            b.cells[i].flag = flag;
+        }
+        b
+    }
+
+    pub fn export_state(&self) -> String {
+        let mut state = String::new();
+        state.push_str(&format!(
+            "{} {} {}\n",
+            self.width, self.height, self.difficulty
+        ));
+        state.push_str(&format!("{}\n", self.seed));
+        state.push_str(
+            &self
+                .cells
+                .iter()
+                .enumerate()
+                .map(|(i, x)| {
+                    let mut c: String = String::new();
+                    if x.reveal {
+                        c.push_str("1");
+                    } else {
+                        c.push_str("0");
+                    }
+                    if x.flag {
+                        c.push_str("1");
+                    } else {
+                        c.push_str("0");
+                    }
+                    c.push_str(" ");
+                    if (i + 1) % self.width == 0 {
+                        c.push_str("\n");
+                    }
+                    c
+                })
+                .collect::<String>(),
+        );
+        state
     }
 
     pub fn with_height(mut self, height: usize) -> Self {
@@ -78,16 +163,26 @@ impl Board {
         self
     }
 
-    pub fn build(mut self) -> Self {
+    pub fn with_seed(mut self, seed: String) -> Self {
+        self.seed = seed;
+        self
+    }
+
+    pub fn build(&mut self) -> Self {
         let width = self.width.clone();
         let height = self.height.clone();
         let difficulty = self.difficulty.clone();
 
-        let mut rng = rand::thread_rng();
+        if self.seed == "".to_string() {
+            self.seed = petname::petname(4, "-")
+        }
+
+        let mut rng: SipRng = Seeder::from(self.seed.clone() + "my_salty_salt").make_rng();
         let mines = rand::seq::index::sample(&mut rng, width * height, difficulty).into_vec();
         let cells: Vec<Cell> = (0..self.width * self.height)
             .into_iter()
             .map(|i| {
+                //println!("{:#?}", self.clone().adjacent_cells(i));
                 let neighbor_bombs: u8 = self
                     .clone()
                     .adjacent_cells(i)
@@ -102,68 +197,69 @@ impl Board {
             })
             .collect();
         self.cells = cells;
-        self
+        self.clone()
     }
 
     fn adjacent_cells(self, i: usize) -> Vec<usize> {
         let width = self.width.clone();
         let height = self.height.clone();
+        let mut tiles: Vec<usize> = Vec::new();
         // It's longer without branching logic, but it saves time.
         if i == 0 {
             // Top left corner
-            [i + 1, i + self.height, i + 1 + self.height].to_vec()
+            tiles = [i + 1, i + self.height, i + 1 + self.height].to_vec();
         } else if width - 1 == i {
             // Top right corner
-            [i - 1, i + self.height, i - 1 + self.height].to_vec()
-        } else if i + 1 == width * height - width {
+            tiles = [i - 1, i + self.height, i - 1 + self.height].to_vec();
+        } else if i == width * height - width {
             // Bottom left corner
-            [i + 1, i - self.width, i + 1 - self.width].to_vec()
+            tiles = [i + 1, i - self.width, i + 1 - self.width].to_vec();
         } else if i + 1 == width * height {
             // Bottom right corner
-            [i - 1, i - self.width, i - 1 - self.width].to_vec()
+            tiles = [i - 1, i - self.width, i - 1 - self.width].to_vec();
         } else if i < width {
             // Top row
-            [
+            tiles = [
                 i - 1,
                 i + 1,
                 i + self.height,
                 i - 1 + self.height,
                 i + 1 + self.height,
             ]
-            .to_vec()
+            .to_vec();
         } else if i > width * (height - 1) {
             // Bottom row
-            [
+            tiles = [
                 i - 1,
                 i + 1,
                 i - self.width,
                 i - 1 - self.width,
                 i + 1 - self.width,
             ]
-            .to_vec()
+            .to_vec();
         } else if i % width == 0 {
             // Left column
-            [
+            tiles = [
                 i + 1,
                 i - self.width,
                 i + 1 - self.width,
                 i + self.height,
                 i + 1 + self.height,
             ]
-            .to_vec()
-        } else if (i + 1) % 5 == 0 {
+            .to_vec();
+        } else if (i + 1) % width == 0 {
             // Right column
-            [
+            tiles = [
                 i - 1,
                 i - self.width,
                 i - 1 - self.width,
                 i + self.height,
                 i - 1 + self.height,
             ]
-            .to_vec()
+            .to_vec();
         } else {
             // Middle of board
-            [
+            tiles = [
                 i - 1,
                 i + 1,
                 i - self.width,
@@ -173,7 +269,121 @@ impl Board {
                 i - 1 + self.height,
                 i + 1 + self.height,
             ]
-            .to_vec()
+            .to_vec();
         }
+        // println!("{}:{:?}", i, tiles);
+        tiles
+    }
+
+    pub fn flag(&mut self, x: usize, y: usize) -> bool {
+        let i = (y * self.width) + x;
+        self.flag_by_index(i)
+    }
+
+    pub fn flag_by_index(&mut self, i: usize) -> bool {
+        self.cells[i].toggle_flag()
+    }
+    pub fn reveal(&mut self, x: usize, y: usize) -> bool {
+        let i = (y * self.width) + x;
+        self.reveal_by_index(i)
+    }
+
+    pub fn reveal_by_index(&mut self, i: usize) -> bool {
+        match self.cells[i].set_reveal(true) {
+            false => false,
+            true => {
+                if self.cells[i].neighbors == 0 {
+                    let neighbors = self.clone().adjacent_cells(i);
+                    for x in neighbors {
+                        if !self.cells[x].reveal {
+                            self.reveal_by_index(x);
+                        }
+                    }
+                }
+                true
+            }
+        }
+    }
+
+    pub fn reveal_all(&mut self) -> bool {
+        for i in 0..self.width * self.height {
+            self.cells[i].set_reveal(true);
+        }
+        true
+    }
+
+    pub fn lost(&self) -> bool {
+        self.cells
+            .iter()
+            .fold(false, |acc, x| acc | x.mine & x.reveal)
+    }
+
+    pub fn won(&self) -> bool {
+        self.cells
+            .iter()
+            .fold(true, |acc, x| acc & ((!x.mine) || x.flag))
+    }
+
+    pub fn export_board(&self) -> String {
+        self.cells
+            .iter()
+            .enumerate()
+            .map(|(i, x)| {
+                let mut c: String = String::new();
+                if !x.reveal {
+                    c.push_str(".");
+                } else if x.mine {
+                    c.push_str("M");
+                } else if x.flag {
+                    c.push_str("F");
+                } else {
+                    c.push_str(&x.neighbors.to_string());
+                }
+                if (i + 1) % self.width == 0 {
+                    c.push_str("\n");
+                }
+                c
+            })
+            .collect()
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut cell_board: String = self
+            .cells
+            .iter()
+            .enumerate()
+            .map(|(i, x)| {
+                let mut c: String = String::new();
+                if i % self.width == 0 {
+                    c.push_str(&(i / self.width).to_string());
+                    c.push_str("|");
+                }
+                if !x.reveal {
+                    c.push_str(".");
+                } else if x.mine {
+                    c.push_str("M");
+                } else if x.flag {
+                    c.push_str("F");
+                } else {
+                    c.push_str(&x.neighbors.to_string());
+                }
+                if (i + 1) % self.width == 0 {
+                    c.push_str("\n");
+                }
+                c
+            })
+            .collect();
+        cell_board.push_str("  ");
+        cell_board.push_str(&(0..self.width).into_iter().map(|_| "-").collect::<String>());
+        cell_board.push_str("\n  ");
+        cell_board.push_str(
+            &(0..self.width)
+                .into_iter()
+                .map(|x| x.to_string())
+                .collect::<String>(),
+        );
+        write!(f, "{}", cell_board)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,59 @@
+#[macro_use]
+extern crate rocket;
+
 mod board;
 
-fn main() {
-    let b = board::Board::new()
-        .with_width(3)
-        .with_height(3)
-        .with_difficulty(1)
-        .build();
-    println!("Minesweepers!");
-    println!("{:#?}", b);
+#[get("/")]
+fn index() -> &'static str {
+    "Minesweeper!"
+}
+
+#[get("/new/<width>/<height>/<difficulty>")]
+fn new(width: usize, height: usize, difficulty: usize) -> String {
+    board::Board::new()
+        .with_width(width)
+        .with_height(height)
+        .with_difficulty(difficulty)
+        .build()
+        .export_state()
+}
+
+#[post("/flag/<x>/<y>?<send_state>", data = "<board_state>")]
+fn flag(x: usize, y: usize, send_state: bool, board_state: String) -> String {
+    let mut b = board::Board::from(board_state);
+    b.flag(x, y);
+    let mut board = b.export_board();
+    println!("{:?}", send_state);
+    if b.won() {
+        board.push_str("Win!");
+    } else if b.lost() {
+        board.push_str("Lose!");
+    }
+    if send_state {
+        board + "\n" + &b.export_state()
+    } else {
+        board
+    }
+}
+
+#[post("/reveal/<x>/<y>?<send_state>", data = "<board_state>")]
+fn reveal(x: usize, y: usize, send_state: bool, board_state: String) -> String {
+    let mut b = board::Board::from(board_state);
+    b.reveal(x, y);
+    let mut board = b.export_board();
+    if b.won() {
+        board.push_str("Win!");
+    } else if b.lost() {
+        board.push_str("Lose!");
+    }
+    if send_state {
+        board + "\n" + &b.export_state()
+    } else {
+        board
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, new, flag, reveal])
 }
